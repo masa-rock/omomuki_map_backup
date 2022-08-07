02f98a2d509a80066fc295a7d23bdb841cdb9924
masa-rock <79382412+masa-rock@users.noreply.github.com>
2022-08-07 21:41:39 +0900

    トップページの作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+// import { BrowserRouter, Router } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Header from './components/modules/Header';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>Hello World</h1>
-      <img src={logo} className="App-logo" alt="logo" />
+
+const TopContainer = () => {
+  const [count,setCount] = useState(0);
+  return(
+    <div>
+      <ContainerStyle>
+        <h1>Hello World</h1>
+      <p>you clicked {count} times</p>
+      <Button variant="contained" color= "primary" onClick={() => setCount(count + 1)}>
+        Click me
+      </Button>
+      <Title>here is big?</Title>
+      </ContainerStyle>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+  <div className="App">
+    <Header />
+    <TopContainer />
+  </div>
+  // <Router>
+  //   <Routes />
+  // </Router>
+  );
+ }
+
+const TopImage = styled.div`
+  color: "#fff";
+  Padding: "200px";
+  BackgroundImage :url(${process.env.PUBLIC_URL}/top_image.jpg);
+`;
+
+const Title = styled.h1`
+ color: #aeaeae;
+ font-size: 80px
+ `;
+
+const ContainerStyle = styled.div`
+  background-image: url(${process.env.PUBLIC_URL}/top_image.jpg);
+  padding: 200px;
+  color: #aeaeae;
+  font-size: 80px
+`;
+
 export default App;
