f396276bc7591406cf8516fbb49613212d1f405c
masa-rock <79382412+masa-rock@users.noreply.github.com>
2022-08-09 00:22:08 +0900

    ログインができないバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       # delete '/todos/destroy_all', to: 'todos#destroy_all'
       resources :tags, only: %i[index show create update]
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
