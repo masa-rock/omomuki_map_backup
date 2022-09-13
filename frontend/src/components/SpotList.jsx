cd0dde2de6aea53c93ded17004bfb7ac054f986c
masa-rock <msxqx464647279@gmail.com>
2022-09-13 22:02:06 +0900

    ページネーション機能追加

--- frontend/src/components/SpotList.jsx
+++ frontend/src/components/SpotList.jsx
@@ -2,14 +2,22 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 import { Grid, CardHeader, Card } from "@material-ui/core";
 import { CardMedia } from "@mui/material";
-import { useNavigate, useLocation } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom";
+import ReactPaginate from 'react-paginate';
 
 export const SpotList = () => {
   const [spots, setSpots] = useState([])
   const navigate = useNavigate()
   const location = useLocation()
   const searchparams = location.state.params
-  
+  const [ offset, setOffset ] = useState(0);
+  const perPage = 5;
+
+  const handlePageChange = (data) => {
+    let page_number = data['selected'];
+    setOffset(page_number*perPage)
+  }
+
   useEffect(() => {
     console.log(searchparams)
     axios.get('http://0.0.0.0:3001/api/v1/posts', {params: searchparams})
@@ -39,7 +47,7 @@ export const SpotList = () => {
       <h3>spotリスト</h3>
       {}
       <Grid container spacing={1}>
-        {spots.map((val) => {
+        {spots.slice(offset, offset+ perPage).map((val) => {
         return(  
           <Grid item xs={3}>
             <Card onClick={() => ToSinglePage(val.id)}>
@@ -56,6 +64,25 @@ export const SpotList = () => {
         )
         })}
       </Grid>
+      <ReactPaginate
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        containerClassName='pagination'
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        pageCount={Math.ceil(spots.length/perPage)}
+        activeClassName='active'
+        onPageChange={handlePageChange}
+        previousLabel='<'
+        nextLabel='>'
+        previousClassName='page-item' // '<'の親要素(li)のクラス名
+        nextClassName='page-item' //'>'の親要素(li)のクラス名
+        previousLinkClassName='page-link'  //'<'のリンクのクラス名
+        nextLinkClassName='page-link'//'>'のリンクのクラス名
+        breakLabel='...'
+        breakClassName='page-item' // 上記の「…」のクラス名
+        breakLinkClassName='page-link' // 「…」の中のリンクにつけるクラス
+      />
     </>
   )
 }
